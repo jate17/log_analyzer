a2a81c0c6c164f64c4801b5b1165b737c3fa0638
jate17 <federicomanfredi.longo@gmail.com>
2025-11-01 22:40:25 +0100

    Setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Read};
+
+
+
+fn main() -> io::Result<()> {
+    let mut content = String::new();
+    File::open("log.txt")?.read_to_string(&mut content);
+    println!("{}", content);
+    Ok(())
 }
